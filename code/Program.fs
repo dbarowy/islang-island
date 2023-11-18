ea52842569eb92f8ab6c1817858c3cff13bbb238
Ammar Eltigani <ammartigani12@gmail.com>
2023-11-18 14:06:27 -0500

    now complete setup

--- code/Program.fs
+++ code/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Parser
+open Evaluator
+open System.IO
+
+[<EntryPoint>]
+let main argv : int =
+    (* Check for proper usage *)
+    if argv.Length <> 1 && argv.Length <> 2 then
+        printfn "Usage: dotnet run <file> [debug]"
+        exit 1
+
+    (* read in the input file *)
+    let file = argv.[0]
+    let input = File.ReadAllText file
+
+    (* does the user want parser debugging turned on? *)
+    let do_debug = if argv.Length = 2 then true else false
+
+    (* try to parse what they gave us *)
+    let ast_maybe = parse input do_debug
+
+    (* try to evaluate what we parsed... or not *)
+    match ast_maybe with
+    | Some ast ->
+        eval ast Map.empty |> ignore
+        0
+    | None     ->
+        printfn "Invalid program."
+        1

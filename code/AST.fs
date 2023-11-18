0d7f263e8bfd8525f0aeb2736ee56a546e94f35c
Ammar Eltigani <ammartigani12@gmail.com>
2023-11-18 16:10:29 -0500

    more edits to AST

--- code/AST.fs
+++ code/AST.fs
@@ -1,15 +1,15 @@
 module AST
 
 type Point = {x: int; y: int}
-type Circle = {x: Point; y: int}
-type Dims = {x: int; y: int}
-type Component =
-| string
-| Circle of Circle
-| Point of Point
 
-type Name = string
-type Definition = {name: Name; dims: Dims; components: Component list}
-type Canvas = Canvas of Definition list * string
+type Dims = {w: int; h: int}
+
+type Component =
+| Name of string
+| Circle of Point * int
+
+type Definition = {name: string; dims: Dims; components: Component list}
+
+type Canvas = Canvas of Definition list
 
 let CANVAS_SZ = 400
